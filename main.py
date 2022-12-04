31da8ad138f81c2151d2fea411dda9863c8c433d
ButtonBill <Willakarra@gmail.com>
2022-12-04 15:12:34 -0500

    Solution to first advent puzzle part 2

--- main.py
+++ main.py
@@ -20,7 +20,20 @@ def mostCalories(fileName):
     highestCalories = max(calorieDict.values())
     mostCaloriesElf = max(calorieDict, key=calorieDict.get)
 
+    calorieDict.pop(mostCaloriesElf)
+    secondHighestCalories = max(calorieDict.values())
+    secondMostCaloriesElf = max(calorieDict, key=calorieDict.get)
+
+    calorieDict.pop(secondMostCaloriesElf)
+    thirdHighestCalories = max(calorieDict.values())
+    thirdMostCaloriesElf = max(calorieDict, key=calorieDict.get)
+
+
+
     print("The elf carrying the most calories is Elf number " + str(mostCaloriesElf) + " carrying " + str(highestCalories))
+    print("The elf carrying the second most calories is Elf number " + str(secondMostCaloriesElf) + " carrying " + str(secondHighestCalories))
+    print("The elf carrying the third most calories is Elf number " + str(thirdMostCaloriesElf) + " carrying " + str(thirdHighestCalories))
+    print("The total number of calories they're carrying is "+ str(highestCalories+secondHighestCalories+thirdHighestCalories))
 
 mostCalories("input.txt")
 
