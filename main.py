4f114e0f6dbba089f069896fc6b4844170bcdeb4
ButtonBill <Willakarra@gmail.com>
2022-12-04 17:44:51 -0500

    Solution to Advent puzzle day 4 part 2

--- main.py
+++ main.py
@@ -11,11 +11,9 @@ def containedPairs(fileName):
         assignments = stripPair.split(",")
         firstElf = assignments[0].split("-")
         secondElf = assignments[1].split("-")
-        if int(firstElf[0]) <= int(secondElf[0]) and int(firstElf[1]) >= int(secondElf[1]):
-            containedCount += 1
-        elif int(secondElf[0]) <= int(firstElf[0]) and int(secondElf[1]) >= int(firstElf[1]):
-            containedCount += 1
-    print("There are " + str(containedCount) + " pairs where one's work fully contains another.")
+        if max(int(firstElf[0]),int(secondElf[0])) <= min(int(firstElf[1]),int(secondElf[1])):
+                containedCount += 1
+    print("There are " + str(containedCount) + " pairs where the work overlaps")
 
 containedPairs("day4.txt")
 
