4a6572192ac0dd346da27ddc5f768e49178e0c00
ButtonBill <Willakarra@gmail.com>
2022-12-04 17:01:26 -0500

    Solution to Advent puzzle day 3 part 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def readData(fileName):
         lines = f.readlines()
     return lines
 
-def prioritiesSum(fileName):
+def prioritiesSumPartOne(fileName):
     rucksacks = readData(fileName)
     prioritySum = 0
     for rucksack in rucksacks:
@@ -15,5 +15,37 @@ def prioritiesSum(fileName):
             priority = ord(commonType[0]) - 96
         prioritySum +=priority
     print ("The sum of the prorities of the shared item types is " + str(prioritySum))
+
+def prioritiesSum(fileName):
+    rucksacks = readData(fileName)
+    groups = []
+    prioritySum = 0
+
+
+    currentList = []
+    currentSack = 1
+    for rucksack in rucksacks:
+        currentList.append(rucksack)
+        if currentSack == 3:
+            groups.append(currentList)
+            currentList = []
+            currentSack = 0
+        currentSack+=1
+    for group in groups:
+        sack1 = group[0].strip()
+        sack2 = group[1].strip()
+        sack3 = group[2].strip()
+        commonFirstTwo = list(set(sack1) & set(sack2))
+        for char in commonFirstTwo:
+            if char in sack3:
+                commonType = char
+        if ord(commonType) in range(65,91):
+            priority = ord(commonType) - 38
+        else:
+            priority = ord(commonType) - 96
+        prioritySum +=priority
+    print("The sum of the prorities of the shared item types is " + str(prioritySum))
+
+
 
 prioritiesSum("day3.txt")
